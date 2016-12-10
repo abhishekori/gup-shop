bf6f4227e55635644f578f44f256cbaf129fdbdc
abhishek <abhikori1994@gmail.com>
2016-12-11 03:31:42 +0530

    testing families feature

--- services/wit.js
+++ services/wit.js
@@ -63,9 +63,10 @@ var actions = {
 		if(!context.familyMembers){
 			var pingUrl="https://graph.facebook.com/v2.6/"+context._fbid_+"?access_token="+Config.FB_PAGE_TOKEN;
 			request(pingUrl,function(error, response, body){
-				console.log(body)
+				//console.log(body)
+				context.familyMembers=[{name:body.first_name,fbid:context._fbid_}];
 			});
-			context.familyMembers=[];
+
 		}
 		if(name){
 			context.name=name;
