08c377a9bd5c005c6805b5d90a9c9f576f0a567b
abhishek <abhikori1994@gmail.com>
2016-12-10 05:53:42 +0530

    inital commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("your app is running");
+var express = require('express');
+var app = express();
+
+app.get("/",function(req,res){
+	res.send("your app is running");
+});
+
+
+app.set('port', (process.env.PORT) || 5000)
+// SPIN UP SERVER
+app.listen(app.get('port'), function () {
+
+  console.log('Running on port', app.get('port'))
+})
