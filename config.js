75e01a77312abd768740f603ed133cf3f6b7328e
abhishek <abhikori1994@gmail.com>
2016-12-10 06:21:26 +0530

    inital commit

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ if (!FB_PAGE_TOKEN) {
 	throw new Error('Missing FB_PAGE_TOKEN. Go to https://developers.facebook.com/docs/pages/access-tokens to get one.')
 }
 
-var FB_VERIFY_TOKEN = 'just_do_it'
+var FB_VERIFY_TOKEN = 'gupshop'
 
 module.exports = {
   WIT_TOKEN: WIT_TOKEN,
