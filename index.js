740f4425becafe769f4db348097072bb3c72e843
abhishek <abhikori1994@gmail.com>
2016-12-10 06:33:34 +0530

    inital commit

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict'
+
 var express = require('express')
 var bodyParser = require('body-parser')
 var request = require('request')
@@ -6,23 +8,32 @@ var Config = require('./config')
 var FB = require('./connectors/facebook')
 var Bot = require('./bot')
 
-var app = express();
 
-app.get("/",function(req,res){
-	res.send("your app is running");
-});
-app.get('/webhooks', function (req, res) {
-    if (req.query['hub.verify_token'] === "gupshop") {
-        res.send(req.query['hub.challenge'])
-    }
-    res.send('Error, wrong token')
-});
+// LETS MAKE A SERVER!
+var app = express()
 app.set('port', (process.env.PORT) || 5000)
 // SPIN UP SERVER
 app.listen(app.get('port'), function () {
 
     console.log('Running on port', app.get('port'))
 })
+// PARSE THE BODY
+app.use(bodyParser.json())
+
+
+// index page
+app.get('/', function (req, res) {
+    res.send('hello world i am a chat bot')
+})
+
+// for facebook to verify
+app.get('/webhooks', function (req, res) {
+    if (req.query['hub.verify_token'] === Config.FB_VERIFY_TOKEN) {
+        res.send(req.query['hub.challenge'])
+    }
+    res.send('Error, wrong token')
+})
+
 // to send messages to facebook
 app.post('/webhooks', function (req, res) {
     var entry = FB.getMessageEntry(req.body)
@@ -48,11 +59,9 @@ app.post('/webhooks', function (req, res) {
             //Bot.read(entry.sender.id, entry.message.text, function (sender, reply) {
             //    FB.newMessage(sender, reply)
             //})
-            FB.newMessage(sender, "your message "+entry.message.text);
+            FB.newMessage(sender, entry.message.text)
         }
     }
 
     res.sendStatus(200)
 })
-
-
